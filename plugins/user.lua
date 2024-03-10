f7bd9c96195b8f592842f71cc888622737e5bc9f
Alcebiades <alcebiades.29@gmail.com>
2024-03-10 12:18:45 -0300

    feat: adding plugins to handle tmux windows

--- plugins/user.lua
+++ plugins/user.lua
@@ -17,4 +17,20 @@ return {
   --   priority = 1000,
   -- },
 
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+
 }
